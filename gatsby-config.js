3575229e4de847f267c85cb31803e40e7e62e521
Andrew Morozow <andrey940@gmail.com>
2021-02-18 20:54:00 +0200

    add(analytics): GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,11 @@
 module.exports = {
-    plugins: [
-
-    ]
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'G-2L909QVF40',
+        head: true,
+      },
+    },
+  ],
 }
