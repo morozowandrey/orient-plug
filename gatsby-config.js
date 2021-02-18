f13e933f1b279e2e2763e70e2223ca33f9e6b909
Andrew Morozow <andrey940@gmail.com>
2021-02-18 21:04:49 +0200

    add(analitics): gtag plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,36 @@
 module.exports = {
   plugins: [
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: 'G-2L909QVF40',
+    //     head: true,
+    //   },
+    // },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: 'G-2L909QVF40',
-        head: true,
+        trackingIds: [
+          'G-2L909QVF40', // Google Analytics / GA
+          //   'AW-CONVERSION_ID', // Google Ads / Adwords / AW
+          //   'DC-FLOODIGHT_ID', // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          //   optimize_id: 'OPT_CONTAINER_ID',
+          //   anonymize_ip: true,
+          //   cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
+        },
       },
     },
   ],
